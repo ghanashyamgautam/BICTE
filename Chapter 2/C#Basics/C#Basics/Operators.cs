28658b7e6a8b2f455397d645be8e8bc12bc34913
piyush pant <epiyushpant@gmail.com>
2025-01-01 09:05:07 +0545

    chapter 2 code qand slides added

---
+++ Chapter 2/C#Basics/C#Basics/Operators.cs
@@ -0,0 +1,24 @@
+﻿namespace C_Basics
+{
+    internal class Operators
+    {
+        internal static void GetOperatorsExercise()
+        {
+            Console.WriteLine("Please enter first number!");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second  number!");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+
+            int addedNumber = firstNumber + secondNumber;
+            Console.WriteLine("Addition : " + addedNumber);
+
+
+            //Task 1 complete remaining code 
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Chapter 2/C#Basics/C#Basics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operators.GetOperatorsExercise();
+        }
+    }
+}
