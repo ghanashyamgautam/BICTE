5d11e24c9049be52bcab833149a518e9106a0dd0
piyush pant <epiyushpant@gmail.com>
2025-04-02 06:39:32 +0545

    correct connection code

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -10,40 +10,37 @@ namespace ContosoUniversity
         {
             var builder = WebApplication.CreateBuilder(args);
 
-           
             // Add services to the container
             builder.Services.AddControllersWithViews();
 
-            // Build the application
-            var app = builder.Build();
-
-
-            if (app.Environment.IsDevelopment())
+            // Register the DbContext BEFORE calling builder.Build()
+            if (builder.Environment.IsDevelopment())
             {
                 // Use connection string from appsettings.Development.json
-                // Register the DbContext
                 builder.Services.AddDbContext<SchoolContext>(options =>
                     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
             }
             else
             {
                 // Use connection string from appsettings.json (Production/Staging)
-                // Register the DbContext
                 builder.Services.AddDbContext<SchoolContext>(options =>
-                    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionConnection")));
 
+            }
+
+            // Build the application AFTER services are registered
+            var app = builder.Build();
+
+            // Exception handling for production
+            if (!app.Environment.IsDevelopment())
+            {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapControllerRoute(
